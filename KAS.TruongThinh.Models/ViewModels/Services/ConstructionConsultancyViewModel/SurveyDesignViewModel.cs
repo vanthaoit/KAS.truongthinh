c59c4cd2433fdfeb449fc584aad5a26b705adb6e
unknown <vietnamthaotranvan@gmail.com>
2020-02-20 00:51:28 +0700

    inititial

---
+++ KAS.TruongThinh.Models/ViewModels/Services/ConstructionConsultancyViewModel/SurveyDesignViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KAS.TruongThinh.Models.ViewModels.Services.ConstructionConsultancyViewModel
+{
+    public class SurveyDesignViewModel
+    {
+    }
+}
